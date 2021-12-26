85b2ac8005760e60ea347e0ffb2db4bbf114ec96
nairoMudumane <nazirmudumane@gmail.com>
2021-12-26 14:42:28 +0200

    first components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+
 function App() {
-  return <div>React Dogs</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
