4f025a339cb9126222fe5ee3a2e0a9446f960f1a
nairoMudumane <nazirmudumane@gmail.com>
2021-12-26 15:48:01 +0200

    testing api

--- src/Components/login/login.js
+++ src/Components/login/login.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LoginCreate from './LoginCreate';
+import LoginForm from './LoginForm';
+import LoginPasswordReset from './LoginPasswordReset';
+import LoginPasswordLost from './LoginPasswordLost';
 
 const Login = () => {
   return (
-    <div>
-      <p>Login</p>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="create" element={<LoginCreate />} />
+        <Route path="reset" element={<LoginPasswordReset />} />
+        <Route path="lost" element={<LoginPasswordLost />} />
+      </Routes>
+    </>
   );
 };
 
